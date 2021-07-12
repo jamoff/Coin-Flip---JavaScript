d29d8bd3d5bd9816e30fe5c54d329f81ba9ddafd
jamoff <87047820+jamoff@users.noreply.github.com>
2021-07-12 07:56:44 -0400

    Update coinflip.js

--- coinflip.js
+++ coinflip.js
@@ -1,1 +1,13 @@
+function coinFlip () {
 
+var counter = parseInt(prompt("How many coin tosses would you like?"));
+
+for (i = 1; i <= counter; i++) {
+
+Math.floor(Math.random() * 2); 
+
+if (Math.floor(Math.random() * 2) == 0) 
+
+{ console.log("Heads") }
+
+else { console.log("Tails") } } } coinFlip()
