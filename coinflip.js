fdd36ba1e8ed858df1e033696cc9e7550d5c5e4f
jamoff <87047820+jamoff@users.noreply.github.com>
2021-07-16 12:38:42 -0400

    Update coinflip.js

--- coinflip.js
+++ coinflip.js
@@ -1,13 +1,75 @@
-function coinFlip () {
+alert("Guess what the computer will flip");
 
-var counter = parseInt(prompt("How many coin tosses would you like?"));
 
-for (i = 1; i <= counter; i++) {
+var player = parseInt(prompt("Press '0' for Heads. Press '1' for Tails"))
 
-Math.floor(Math.random() * 2); 
+while (player > 1 || player < 0) {
+  console.log("error")
+  var player = parseInt(prompt("Choose a number"))
+}
+var computer = Math.floor(Math.random() * 2);
 
-if (Math.floor(Math.random() * 2) == 0) 
+var playerResult = 0;
+var computerResult = 0;
 
-{ console.log("Heads") }
+if (player == 0 && computer == 0) {
+  console.log("You guessed Heads. Good job!")
+  playerResult += 1;
+}
 
-else { console.log("Tails") } } } coinFlip()
+else if (player == 1 && computer == 1) {
+  console.log("You guessed Tails. Good job!")
+  playerResult += 1;
+}
+
+else if (player == 0 && computer == 1) {
+  console.log("Your result did not match the computer's :(")
+  computerResult += 1;
+}
+
+else if (player == 1 && computer == 0) {
+  console.log("Your result did not match the computer's :(")
+  computerResult += 1;
+}
+
+var question = prompt("Would you like to play again?")
+
+while (question === "Y") {
+  var player = parseInt(prompt("Press '0' for Heads. Press '1' for Tails"))
+
+  while (player > 1 || player < 0) {
+    console.log("error")
+    var player = parseInt(prompt("Choose a number"))
+  }
+  var computer = Math.floor(Math.random() * 2);
+
+  if (player == 0 && computer == 0) {
+    console.log("You guessed Heads. Good job!")
+    playerResult += 1;
+  }
+
+  else if (player == 1 && computer == 1) {
+    console.log("You guessed Tails. Good job!")
+    playerResult += 1;
+  }
+
+  else if (player == 0 && computer == 1) {
+    console.log("Your result did not match the computer's :(")
+    computerResult += 1;
+  }
+
+  else if (player == 1 && computer == 0) {
+    console.log("Your result did not match the computer's :(")
+    computerResult += 1;
+  }
+
+  var question = prompt("Would you like to play again?")
+
+
+if (question === "N") {
+  break;
+
+}
+}
+
+console.log("The final score for the player is",playerResult,"and the final result for the computer is",computerResult)
